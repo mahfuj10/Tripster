936a07fb27fa580af1e7e10611436d035693b98c
mahfuj10 <mahfujglobal@gmail.com>
2023-02-13 15:22:42 +0600

    added files

---
+++ client/src/pages/Dashboard/DashboardDefault/DashboardDefault.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import video from './video/videoNature.mp4';
+import "./DashboardDefault.css"
+
+const DashboardDefault = () => {
+
+    return (
+        <div style={{ backgroundImage: 'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1574&q=80', width: '100vw' }}>
+            <img alt='image' style={{ width: '100vw' }} src="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1574&q=80" />
+        </div >
+    );
+};
+
+export default DashboardDefault;
---
+++ client/src/pages/Home/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import image1 from '../../../images/image1.jpg'
+import image2 from '../../../images/image2.jpg'
+import image3 from '../../../images/image3.jpg'
+
+const Header = () => {
+    return (
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div className='flex items-center justify-center' style={{ backgroundImage: `url(${image2})`, height: '94vh', width: "100vw", backgroundSize: 'cover', backgroundAttachment: 'fixed', display: 'flex ', alignItems: 'center', justifyContent: 'center' }} class="carousel-item active">
+                    <h1 style={{ fontFamily: 'Nunito, sans-serif' }} className='text-6xl text-white text-semibold text-center'>
+                        My Daily Travel Blog To Inspire And  <br />Get You Motivated!
+                    </h1>
+                </div>
+                <div style={{ backgroundImage: `url(${image2})`, height: '94vh', width: "100vw", backgroundSize: 'cover', backgroundAttachment: 'fixed', display: 'flex ', alignItems: 'center', justifyContent: 'center' }} class="carousel-item" >
+
+                </div>
+                <div class="carousel-item" style={{ backgroundImage: `url(${image2})`, height: '94vh', width: "100vw", backgroundSize: 'cover', display: 'flex ', alignItems: 'center', justifyContent: 'center', backgroundAttachment: 'fixed' }} >
+                    <h1 style={{ fontFamily: 'Nunito, sans-serif' }} className='text-6xl text-white text-semibold text-center'>
+                        My Daily Travel Blog To Inspire And  <br />Get You Motivated!
+                    </h1>
+                </div>
+            </div>
+            {/* <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button> */}
+        </div>
+    );
+};
+
+export default Header;
